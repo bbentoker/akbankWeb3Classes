f9df747f5ecddda7f5e0821938b7e3814491b8b6
Toker <bbentoker@gmail.com>
2022-08-29 00:07:33 +0300

    .

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { Booth } from "./Booth.js";
 import { Administration } from "./Administration.js";
 
 const car = new Car(1,"Emir","Toker",100);
-const bus = new Bus(2,"Emir","Toker",400);
+const bus = new Bus(2,"Hakan","Akkaş",400);
 
 const booth = new Booth();
 
